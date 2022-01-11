011e5a1a1bf0345f747fe372dec99864fb417887
mprbowo <mprabowo75@gmail.com>
2022-01-11 11:22:16 +0700

    Proyek 2

---
+++ admin/application/views/layout/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer dark-bg">
+    <div class="pull-right hidden-xs"></div>
+     Copyright &copy; 2019 Lembaga Manajemen Infaq | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+	 
+    
+    <div class="col-md-6 text-right">
+     <b>Created by KP ITATS Team</b> |
+    </div>
+</footer>
